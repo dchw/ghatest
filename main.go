0efb1be07266205473d3bd7604195853a1a50f24
Corey Larson <corey@earthly.dev>
2024-10-01 06:58:20 -0600

    test

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("*** Passthrough ***")
+	
+	cmd := exec.Command("Runner.Worker.Legit", os.Args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("passthrough failed: %s\n", err.Error()) // handle error
+	}
+
+	os.Exit(cmd.ProcessState.ExitCode())
+}
